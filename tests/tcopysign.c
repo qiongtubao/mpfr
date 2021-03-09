e7278dcb62196034eebc2d9c3a5b5c1ec16c92bd
Vincent Lefèvre <vincent@vinc17.net>
2021-03-09 12:45:39 +0000

    In mpfr.h, fixed some macros implementing functions. Added tests.
    * src/mpfr.h:
        - Macros mpfr_nan_p, mpfr_inf_p, mpfr_zero_p, mpfr_regular_p,
          mpfr_get_prec, mpfr_get_exp, mpfr_copysign and mpfr_signbit
          yielded a compilation error when the argument (the 3rd one
          for mpfr_copysign) was of type void *, for instance.
        - Macro mpfr_set had the same issue with the 2nd argument, but
          also evaluated this argument twice. The fix was possible only
          with a GNU extension. Thus this macro is available only with
          GCC and compatible compilers (see __GNUC__); otherwise, the
          mpfr_set function is used.
    * src/ubf.c: added a missing cast (this issue became visible with
      the fix of the mpfr_get_prec macro).
    * tests/mpfr-test.h: added macros for the new tests (tests with
      void * arguments are invalid in C++ and they trigger a warning
      with GCC's -Wc++-compat, so we need to avoid these issues).
    * tests/*.c: added tests to check that some calls (potentially
      macros) behave like function calls. In detail:
        - tcopysign.c: mpfr_copysign and mpfr_setsign.
        - texceptions.c: mpfr_get_prec.
        - tisnan.c: mpfr_nan_p, mpfr_inf_p, mpfr_number_p (though not
          currently concerned here), mpfr_zero_p and mpfr_regular_p.
        - tset.c: mpfr_set.
        - tset_exp.c: mpfr_get_exp.
    * doc/mpfr.texi: added some text in Section "Custom Interface" to
      clarify the usage of macros for this interface.
    
    (merged changesets r14447-14463 from the trunk)
    
    [From SVN r14469 (branches/4.1)]

--- tests/tcopysign.c
+++ tests/tcopysign.c
@@ -27,16 +27,17 @@ copysign_variant (mpfr_ptr z, mpfr_srcptr x, mpfr_srcptr y,
                   mpfr_rnd_t rnd_mode, int k)
 {
   mpfr_srcptr p;
+  int a = 0, b = 0, c = 0;
 
   /* invalid sign, to test that the sign is always correctly set */
   MPFR_SIGN (z) = 0;
 
-  if (k >= 6)
+  if (k >= 8)
     {
       MPFR_ASSERTN (MPFR_PREC (z) >= MPFR_PREC (x));
       mpfr_set (z, x, MPFR_RNDN);
       p = z;
-      k -= 6;
+      k -= 8;
     }
   else
     p = x;
@@ -51,16 +52,46 @@ copysign_variant (mpfr_ptr z, mpfr_srcptr x, mpfr_srcptr y,
       (mpfr_copysign) (z, p, y, rnd_mode);
       return;
     case 2:
-      mpfr_setsign (z, p, mpfr_signbit (y), rnd_mode);
+#ifdef IGNORE_CPP_COMPAT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wc++-compat"
+#endif
+      mpfr_copysign ((a++, VOIDP_CAST(z)),
+                     (b++, VOIDP_CAST(p)),
+                     (c++, VOIDP_CAST(y)), rnd_mode);
+#ifdef IGNORE_CPP_COMPAT
+#pragma GCC diagnostic pop
+#endif
+      MPFR_ASSERTN (a == 1);
+      MPFR_ASSERTN (b == 1);
+      MPFR_ASSERTN (c == 1);
       return;
     case 3:
-      mpfr_setsign (z, p, (mpfr_signbit) (y), rnd_mode);
+      mpfr_setsign (z, p, mpfr_signbit (y), rnd_mode);
       return;
     case 4:
-      (mpfr_setsign) (z, p, mpfr_signbit (y), rnd_mode);
+      mpfr_setsign (z, p, (mpfr_signbit) (y), rnd_mode);
       return;
     case 5:
+      (mpfr_setsign) (z, p, mpfr_signbit (y), rnd_mode);
+      return;
+    case 6:
       (mpfr_setsign) (z, p, (mpfr_signbit) (y), rnd_mode);
+      return;
+    case 7:
+#ifdef IGNORE_CPP_COMPAT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wc++-compat"
+#endif
+      mpfr_setsign ((a++, VOIDP_CAST(z)),
+                    (b++, VOIDP_CAST(p)),
+                    mpfr_signbit ((c++, VOIDP_CAST(y))), rnd_mode);
+#ifdef IGNORE_CPP_COMPAT
+#pragma GCC diagnostic pop
+#endif
+      MPFR_ASSERTN (a == 1);
+      MPFR_ASSERTN (b == 1);
+      MPFR_ASSERTN (c == 1);
       return;
     }
 }
@@ -79,7 +110,7 @@ main (void)
 
   for (i = 0; i <= 1; i++)
     for (j = 0; j <= 1; j++)
-      for (k = 0; k < 12; k++)
+      for (k = 0; k < 16; k++)
         {
           mpfr_set_nan (x);
           i ? MPFR_SET_NEG (x) : MPFR_SET_POS (x);
--- tests/texceptions.c
+++ tests/texceptions.c
@@ -103,10 +103,26 @@ static void
 check_get_prec (void)
 {
   mpfr_t x;
+  int i = 0;
 
   mpfr_init2 (x, 17);
-  if (mpfr_get_prec (x) != 17 || (mpfr_get_prec)(x) != 17)
+
+  if (mpfr_get_prec (x) != 17 || (mpfr_get_prec) (x) != 17)
     PRINT_ERROR ("mpfr_get_prec");
+
+#ifdef IGNORE_CPP_COMPAT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wc++-compat"
+#endif
+
+  if (mpfr_get_prec ((i++, VOIDP_CAST(x))) != 17)
+    PRINT_ERROR ("mpfr_get_prec (2)");
+
+#ifdef IGNORE_CPP_COMPAT
+#pragma GCC diagnostic pop
+#endif
+
+  MPFR_ASSERTN (i == 1);
   mpfr_clear (x);
 }
 
