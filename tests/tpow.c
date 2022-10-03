ab8abc023d76b7f9492c2e1934a7f4e5dc0fd7d2
Vincent Lefevre <vincent@vinc17.net>
2022-10-03 12:27:54 +0200

    Updated URLs.
    Cherry picked from commits
      56842a7d093c09c148633a37a5a29715b54e90e7
      87b346557fd256f7451f7fe1ce025273e485d6de
      be8d67d7cfd42ebf228f6e4638483a844296e4d2

--- tests/tpow.c
+++ tests/tpow.c
@@ -587,7 +587,7 @@ special (void)
       exit (1);
     }
 
-  /* From http://www.terra.es/personal9/ismaeljc/hall.htm */
+  /* From https://web.archive.org/web/20050824044408/http://www.terra.es/personal9/ismaeljc/hall.htm */
   mpfr_set_prec (x, 113);
   mpfr_set_prec (y, 2);
   mpfr_set_prec (z, 169);
